539ea93af68ad8bf8aacd096546926750efb12a7
Rishmish <rish.mish.2607@gmail.com>
2021-11-13 07:28:13 +0530

    Fix: button issue

--- neog-practice/scripts.js
+++ neog-practice/scripts.js
@@ -5,6 +5,6 @@ console.log(btn[0]);
 
 const arr = [0, 1, 2];
 function clickHandler(e) {
-  console.log(e);
+  console.log(evnet);
 }
 arr.map((item) => btn[item].addEventListener("click", clickHandler));
